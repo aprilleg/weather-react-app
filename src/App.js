075c8571f05969ef6ece7a3ec0d5c5ba201287b3
aprilleg <aprilleguevarra1@gmail.com>
2023-08-02 13:06:28 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        This was coded by Aprille Guevarra, and is <a href="#">open-sourced on Github</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
